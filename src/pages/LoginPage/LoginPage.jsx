262588c41ad2d01b4e40eb56ded4e1043a8249e8
osaydayoub <osaydayoub@gmail.com>
2024-10-31 21:24:54 +0200

    add useEffect to LoginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './LoginPage.css'
 import Login from '../../components/Login/Login';
 import SignUp from '../../components/SignUp/SignUp';
 import loginImg from "../../assets/images/loginImg.jpg";
-
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext'; 
 
 
 function LoginPage() {
   const [login, signUp] = ['login', 'signUp']
   const [selectedComponent, setSelectedComponent] = useState(login);
+  const { isLoggedIn } = useAuth(); 
+  const navigate = useNavigate(); // Initialize the navigate function
+  // Redirect to homepage if already logged in
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/'); // Redirect to the homepage
+    }
+  }, [isLoggedIn, navigate]); 
+  
   function handleComponentChange() {
     if (selectedComponent === login) {
       setSelectedComponent(signUp);
