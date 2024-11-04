30c3ed8cfa4715bb3ad3a22cdcb7443f2340e832
osaydayoub <osaydayoub@gmail.com>
2024-11-04 22:35:10 +0200

    Refactor MealsPage and MealCard to use more MUI components

--- src/components/MealCard/MealCard.jsx
+++ src/components/MealCard/MealCard.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./MealCard.css";
 import { useNavigate } from "react-router-dom";
-import CheckIcon from '@mui/icons-material/Check';
 
 import img1 from "../../assets/images/1.jpg";
 import img2 from "../../assets/images/2.jpg";
 import img3 from "../../assets/images/3.jpg";
 import img4 from "../../assets/images/4.jpg";
-import { Button } from "@mui/material";
+import { Paper } from "@mui/material";
 function getTheImg(img) {
   switch (img) {
     case "1.jpg":
@@ -29,17 +28,15 @@ function MealCard({ meal }) {
     navigate(`/meals/${name.toLowerCase()}`);
   };
   return (
-    <div className="card-container">
+    <Paper
+      className="card-container"
+      sx={{ m: 2, p: 2, textAlign: "center", cursor: "pointer" }}
+      elevation={4}
+      onClick={() => handleSelect(meal.name)}
+    >
       <img className="card-img" src={getTheImg(meal.img)} alt="img" />
       <h2>{meal.name}</h2>
-
-      <div>
-        {/* <button className="meal-btn" onClick={() => handleSelect(meal.name)}>Select!</button> */}
-        <Button variant="contained" endIcon={<CheckIcon />} onClick={() => handleSelect(meal.name)}>
-  Select
-</Button>
-      </div>
-    </div>
+    </Paper>
   );
 }
 
--- src/pages/MealsPage/MealsPage.jsx
+++ src/pages/MealsPage/MealsPage.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./MealsPage.css";
 import MealCard from "../../components/MealCard/MealCard";
+import {
+  Box,
+  Typography,
+} from "@mui/material";
 function MealsPage() {
   const mealsData = [
     { name: "Breakfast", img: "1.jpg" },
@@ -10,14 +14,31 @@ function MealsPage() {
   ];
   return (
     <div className="MealsPage page">
-      <div className="meals-container">
-        <h1>Meals for Today</h1>
-        <p>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: 4,
+          gap: 3,
+          backgroundColor: "#f5f5f5",
+          borderRadius: 2,
+          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+          maxWidth: 600,
+          margin: "0 auto",
+          mt: 1,
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          {`Meals for Today`}
+        </Typography>
+        <Typography gutterBottom>
           Feel free to choose any meal from the four options - Breakfast, Lunch,
           Snack, or Dinner. Customize your Daily Tracking with the meals that
           best suit your preferences and nutritional goals
-        </p>
-      </div>
+        </Typography>
+      </Box>
+
       <div className="cards-container" id="cards-id">
         {mealsData.map((meal, index) => {
           return <MealCard key={index} meal={meal} />;
